605da39a0357b8be203aeb39bc065a932fbdfc1f
nosupe4u <Nosupe4u@lovells-macbook.local>
2016-08-13 23:56:08 -0700

    Initial Commit

---
+++ Gotit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gotit
+//
+//  Created by Nosupe4u on 8/13/16.
+//  Copyright © 2016 Nosupe4u. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
